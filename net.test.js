491351802edcb6171ba2a9ca361865e78e79d3c1
Neto test <anastasiya.parfinenko@gmail.com>
2024-03-13 13:50:59 +0600

    testsPuppeteer

--- net.test.js
+++ net.test.js
@@ -1,10 +1,18 @@
-const { clickElement, putText, getText } = require("./lib/commands.js");
+const {
+  clickElement,
+  putText,
+  getText,
+  chooseFreePlace,
+  chooseNotFreePlace,
+  clickNextDate,
+} = require("./lib/commands.js");
 const { generateName } = require("./lib/util.js");
 
 let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
+  await page.goto("https://qamid.tmweb.ru/client/index.php");
   await page.setDefaultNavigationTimeout(0);
 });
 
@@ -12,44 +20,35 @@ afterEach(() => {
   page.close();
 });
 
-describe("Netology.ru tests", () => {
-  beforeEach(async () => {
-    page = await browser.newPage();
-    await page.goto("https://netology.ru");
-  });
+describe("Booking a place", () => {
+  test("Happy path: бронирование свободного места", async () => {
+    await clickNextDate(page);
+    await clickElement(page, "a[data-seance-id='187']");
+    await chooseFreePlace(page);
+    await clickElement(page, ".acceptin-button");
+    const actual = await getText(page, ".acceptin-button");
+    expect(actual).toEqual("Получить код бронирования");
+  }, 50000);
 
-  test("The first test'", async () => {
-    const title = await page.title();
-    console.log("Page title: " + title);
-    await clickElement(page, "header a + a");
-    const title2 = await page.title();
-    console.log("Page title: " + title2);
-    const pageList = await browser.newPage();
-    await pageList.goto("https://netology.ru/navigation");
-    await pageList.waitForSelector("h1");
-  });
+  test("Happy path: бронирование нескольких свободных мест", async () => {
+    await clickNextDate(page);
+    await clickElement(page, "a[data-seance-id='187']");
+    await chooseFreePlace(page);
+    await chooseFreePlace(page);
+    await chooseFreePlace(page);
+    await clickElement(page, ".acceptin-button");
+    const actual = await getText(page, ".acceptin-button");
+    expect(actual).toEqual("Получить код бронирования");
+  }, 50000);
 
-  test("The first link text 'Медиа Нетологии'", async () => {
-    const actual = await getText(page, "header a + a");
-    expect(actual).toContain("Медиа Нетологии");
-  });
-
-  test("The first link leads on 'Медиа' page", async () => {
-    await clickElement(page, "header a + a");
-    const actual = await getText(page, ".logo__media");
-    await expect(actual).toContain("Медиа");
-  });
-});
-
-test("Should look for a course", async () => {
-  await page.goto("https://netology.ru/navigation");
-  await putText(page, "input", "тестировщик");
-  const actual = await page.$eval("a[data-name]", (link) => link.textContent);
-  const expected = "Тестировщик ПО";
-  expect(actual).toContain(expected);
-});
-
-test("Should show warning if login is not email", async () => {
-  await page.goto("https://netology.ru/?modal=sign_in");
-  await putText(page, 'input[type="email"]', generateName(5));
+  test("Sad path: невозможно забронировать уже занятое место", async () => {
+    await clickNextDate(page);
+    await clickElement(page, "a[data-seance-id='187']");
+    await chooseNotFreePlace(page);
+    await clickElement(page, ".acceptin-button");
+    const actual = await getText(page, ".acceptin-button");
+    expect(actual).toEqual(
+      "Выбраное место занято. Попробуйте выбрать другое место."
+    );
+  }, 50000);
 });
